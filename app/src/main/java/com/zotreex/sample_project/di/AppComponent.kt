7f7f749230ac76a02f363d9a4bc23c340079335d
chudinoff <zotreex@gmail.com>
2022-05-03 18:16:07 +0500

    init Ui

--- app/src/main/java/com/zotreex/sample_project/di/AppComponent.kt
+++ app/src/main/java/com/zotreex/sample_project/di/AppComponent.kt
@@ -1,57 +1,15 @@
 package com.zotreex.sample_project.di
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.zotreex.sample_project.domain.api.SampleService
-import com.zotreex.sample_project.domain.repository.SampleRepository
-import com.zotreex.sample_project.domain.repository.SampleRepositoryImpl
-import com.zotreex.sample_project.domain.usecases.getSampleItem.GetSampleItemUseCase
-import com.zotreex.sample_project.domain.usecases.getSampleItem.GetSampleItemUseCaseImpl
-import com.zotreex.sample_project.domain.usecases.getSampleList.GetSampleListUseCase
-import com.zotreex.sample_project.domain.usecases.getSampleList.GetSampleListUseCaseImpl
-import dagger.Binds
+import com.zotreex.sample_project.ui.SampleFragment
 import dagger.Component
 import dagger.Module
-import dagger.Provides
-import dagger.multibindings.IntoMap
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 @Component(modules = [AppModule::class])
 interface AppComponent {
+    fun inject(fragment: SampleFragment)
 
+    fun viewModelFactory() : ViewModelFactory
 }
 
-@Module(includes = [NetworkModule::class, AppBindModule::class])
+@Module(includes = [NetworkModule::class, AppBindModule::class, ViewModelModule::class])
 class AppModule
-
-@Module
-abstract class ViewModelModule {
-    @Binds
-    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
-}
-
-@Module
-interface AppBindModule {
-    @Binds
-    fun bindSampleRepository(repository: SampleRepositoryImpl): SampleRepository
-
-    @Binds
-    fun bindGetSampleListUseCase(useCase: GetSampleListUseCaseImpl): GetSampleListUseCase
-
-    @Binds
-    fun bindGetSampleItemUseCase(useCase: GetSampleItemUseCaseImpl): GetSampleItemUseCase
-}
-
-@Module
-class NetworkModule {
-    @Provides
-    fun provideSampleApi(): SampleService {
-        val retrofit = Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl("https://www.sampleapi.com/")
-            .build()
-        return retrofit.create()
-    }
-}
