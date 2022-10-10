2c2a9b43e4341224d7a1d1d178402a1b5233863e
exceq <mihail.petuxov@yandex.ru>
2022-10-10 18:14:26 +0500

    renaming project

--- app/src/main/java/com/iit/project/MainActivity.kt
+++ app/src/main/java/com/iit/project/MainActivity.kt
@@ -2,6 +2,7 @@ package com.iit.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.iit.project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
